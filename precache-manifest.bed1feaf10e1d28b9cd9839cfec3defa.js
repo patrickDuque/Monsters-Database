fb25b30ccfe6c02375d1aa777ca973fee196f87b
Patrick Duque <patpatduque26@gmail.com>
2020-04-07 20:29:31 +0800

    Updates

---
+++ precache-manifest.bed1feaf10e1d28b9cd9839cfec3defa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37fcfbcec64a016347d945f32d4c710c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d593648f47b9b5fc69b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4050ac1afd388db6cc7e",
+    "url": "/static/js/2.62478659.chunk.js"
+  },
+  {
+    "revision": "1d593648f47b9b5fc69b",
+    "url": "/static/js/main.76c3c55a.chunk.js"
+  },
+  {
+    "revision": "50cb9aa5f15f329e71d6",
+    "url": "/static/js/runtime-main.576dec9c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
